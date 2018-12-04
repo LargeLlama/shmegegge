bbf8271acf2e4dc3adf3c5ef979fb0f34ec5b92d
Rubin Peci <rubinpeci@gmail.com>
2018-12-04 14:31:42 -0500

    change includes for no reason lol

--- server.c
+++ server.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 int main()
 {
